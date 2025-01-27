e1c187f3defe1e32a5a5369d0a76bdbea5f97e11
Assil28 <assil.dkhil28@gmail.com>
2025-01-27 19:10:37 +0100

    Offre DTO

---
+++ SherElec-Back-end/DTOs/Request/OffreRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace SherElec_Back_end.DTOs.Request
+{
+    public class OffreRequestDTO
+    {
+        public int Quantite { get; set; }
+
+        public string Type { get; set; }
+
+        public bool VendDetails { get; set; }
+        public double PrixKw { get; set; }
+        public DateOnly Date { get; set; }
+
+        public bool Status { get; set; }
+
+        public int Userid { get; set; }
+    }
+}
