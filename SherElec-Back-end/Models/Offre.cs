11e70267d7b176d15e8240765f69ba04ba4fa453
Assil28 <assil.dkhil28@gmail.com>
2025-01-27 19:05:27 +0100

    Offre and User Models

---
+++ SherElec-Back-end/Models/Offre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SherElec_Back_end.Models
+{
+    public class Offre
+    {
+            [Key]
+            public int ID { get; set; }
+
+            public required int UserID { get; set; }
+            public User User { get; set; }
+            public required double Quantite { get; set; }
+            public required string Type { get; set; }
+            public required double PrixKw { get; set; }
+            public required bool VendDetails { get; set; }
+            public required bool Status { get; set; }
+            public required DateOnly Date { get; set; }
+        }
+    }
+}
