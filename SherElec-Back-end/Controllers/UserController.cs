9854eb0cd52f894c963de2f7da902f7c8bbf3fc0
Assil28 <assil.dkhil28@gmail.com>
2025-02-14 20:37:20 +0100

    add userController

---
+++ SherElec-Back-end/Controllers/UserController.cs
@@ -0,0 +1,172 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using SherElec_Back_end.DTOs.Request;
+using SherElec_Back_end.DTOs.Response;
+using SherElec_Back_end.Services.Interfaces;
+
+namespace SherElec_Back_end.Controllers
+{
+    //[EnableCors("AllowAngularDevServer")]
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> InitiateVerification([FromBody] UserRequestDTO userRequestDTO)
+        {
+            if (userRequestDTO == null)
+            {
+                return BadRequest("Les données de l'utilisateur sont incorrectes.");
+            }
+
+            try
+            {
+                await _userService.InitiateEmailVerification(userRequestDTO);
+                return Ok(new { message = "Un code de vérification a été envoyé à votre email." });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Une erreur est survenue", details = ex.Message });
+            }
+        }
+
+        [HttpPost("veriferEmail")]
+        public async Task<IActionResult> VerifyEmail([FromBody] VerificationRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Les données de vérification sont incorrectes.");
+            }
+
+            try
+            {
+                bool isVerified = await _userService.VerifyEmailAndCreateUser(
+                    request.Email,
+                    request.Code
+                );
+
+                if (!isVerified)
+                {
+                    return BadRequest("Code de vérification invalide ou expiré.");
+                }
+
+                return Ok(new { message = "Compte créé avec succès." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Une erreur est survenue", details = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var utilisateur = await _userService.AuthentifierUtilisateurAsync(request.Email, request.Password);
+                var token = _userService.GenererToken(utilisateur);
+                return Ok(new
+                {
+                    Token = token,
+                    Utilisateur = utilisateur // Retourne le DTO pour afficher ces information 
+                });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new { Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = ex.Message });
+            }
+        }
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserRespenseDTO>> GetUserInfo(int id)
+        {
+            var userIdFromToken = User.FindFirst("Id")?.Value;
+
+
+            if (userIdFromToken != id.ToString())
+            {
+                return Unauthorized("Vous ne pouvez recuperer que les informations de votre compte.");
+            }
+
+            var utilisateur = await _userService.GetUserInfo(id);
+
+            if (utilisateur == null)
+            {
+                return NotFound("Utilisateur non trouve.");
+            }
+
+            return Ok(utilisateur);
+        }
+
+
+        [Authorize]
+        [HttpPut("maj/{id}")]
+        public async Task<ActionResult<UserRespenseDTO>> UpdateUser(int id, [FromBody] UserRequestDTO userRequestDto)
+        {
+            if (userRequestDto == null)
+            {
+                return BadRequest("Les donnees de l'utilisateur sont incorrectes.");
+            }
+
+            // Récupérer l'ID utilisateur à partir du jeton JWT
+            var userIdFromToken = User.FindFirst("Id")?.Value;
+
+            // Comparer les IDs : si l'ID dans le jeton ne correspond pas à l'ID dans l'URL, refuser la modification
+            if (userIdFromToken != id.ToString())
+            {
+                return Unauthorized("Vous  pouvez modifier que votre  compte.");
+            }
+
+
+            var updatedUser = await _userService.UpdateUserAsync(id, userRequestDto);
+
+
+            if (updatedUser == null)
+            {
+                return NotFound($"utilisateur avec l'ID {id} non trouve.");
+            }
+
+
+            return Ok(updatedUser);
+        }
+
+
+        [Authorize]
+        [HttpDelete("del/{id}")]
+        public async Task<ActionResult> RemoveUser(int id)
+        {
+            var UserIdFromToken = User.FindFirst("Id")?.Value;
+            if (UserIdFromToken != id.ToString())
+            {
+                return Unauthorized("vous pouvez pas summprimer un compte qui pas le votre");
+            }
+
+
+            var deletedUser = _userService.removeUser(id);
+            if (deletedUser == null)
+            {
+                return NotFound($"utilisateur avec l'ID {id} non trouve.");
+            }
+            return Ok();
+        }
+
+
+    }
+}
