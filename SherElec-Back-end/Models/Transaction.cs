2026795fe354364a763d61e79956ca0f3fc8b48e
Badr Boukries <148701413+Badr8Boukries@users.noreply.github.com>
2025-03-20 16:24:39 +0100

    feat : ajout de Model de transaction

---
+++ SherElec-Back-end/Models/Transaction.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SherElec_Back_end.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [ForeignKey("Acheteur")]
+        public int IdAcheteur { get; set; }
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public User Acheteur { get; set; }
+
+        [Required]
+        [ForeignKey("Vendeur")]
+        public int IdVendeur { get; set; }
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public User Vendeur { get; set; }
+
+        [Required]
+        public double Quantite { get; set; }
+
+        [Required]
+        public double PrixUnitaire { get; set; }
+
+        [Required]
+        public double PrixTotal { get; set; }
+
+        [Required]
+        public DateTime DateTransaction { get; set; }
+
+        [ForeignKey("Offre")]
+        public int? OffreId { get; set; }
+        public Offre? Offre { get; set; }
+    }
+}
